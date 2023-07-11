956bc90b8bdd6ab64d49bb553305b0e0e5751c13
Ivan <kulkov_2003@bk.ru>
2023-07-11 17:22:19 +0300

    add adaptive in slide

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-let offset = 0; // смещение от левого края
-const sliderLine = document.querySelector('.slider-line'); 
+const images = document.querySelectorAll('.slide');
+const sliderLine = document.querySelector('.slider-line');
+let count = 0;
+let width;
 
-document.querySelector('.slider-prev').addEventListener('click', function(){
-    offset += 256;
-    if(offset > 0){
-        offset = -1024
-    }
-    sliderLine.style.left = offset + 'px';
-})
 
-document.querySelector('.slider-next').addEventListener('click', function(){
-    offset -= 256;
-    if(offset < -1024){
-        offset = 0
+
+
+init();
+window.addEventListener('resize', init);
+
+function init(){
+    width = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollSlide();
+}
+
+function rollSlide(){
+    if(count > images.length - 1){
+        count = 0;
     }
-    sliderLine.style.left = offset + 'px';
-});
+    if(count < 0){
+        count = images.length - 1;
+    }
+    sliderLine.style.transform = 'translate(-'+count*width+'px';
+}
+
+    document.querySelector('.slider-next').addEventListener('click', (e) => {
+        count++;
+        rollSlide()
+    });
+    document.querySelector('.slider-prev').addEventListener('click', (e) => {
+        count--;
+        rollSlide()
+    });

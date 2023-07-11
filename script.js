90b0b167ffad05521dab11a0e23fb2339d5c656b
Ivan <kulkov_2003@bk.ru>
2023-07-11 18:19:10 +0300

    add simple swipe in slider

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const images = document.querySelectorAll('.slide');
 const sliderLine = document.querySelector('.slider-line');
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
 let count = 0;
 let width;
 
+let x1 = null;
+let y1 = null;
 
 
 
@@ -27,6 +31,37 @@ function rollSlide(){
         count = images.length - 1;
     }
     sliderLine.style.transform = 'translate(-'+count*width+'px';
+}
+
+function handleTouchStart(event){
+    const firstTouch = event.touches[0];
+    console.log(firstTouch);
+    x1 = firstTouch.clientX;
+    y1 = firstTouch.clientY;
+}
+
+function handleTouchMove(event){
+    if(!x1 || !y1){
+        return false
+    }
+    let x2 = event.touches[0].clientX;
+    console.log(x2)
+    let xDiff = x2 - x1;
+    if(xDiff > 0){
+        let sas = width * count + xDiff;
+        console.log(sas)
+        sliderLine.style.transform = 'translate('+sas+'px';
+        count--;
+        rollSlide()  
+    }
+    else{
+        let sas = width * count + xDiff;
+        console.log(sas)
+        sliderLine.style.transform = 'translate('+-sas+'px';
+        count++;
+        rollSlide() 
+    }
+
 }
 
     document.querySelector('.slider-next').addEventListener('click', (e) => {
